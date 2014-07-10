22444cb951decb735469e9f715027c5302090051
aastepanek <alexisstepanek@Alexis-MacBook-Pro.local>
2014-07-10 16:41:05 -0400

    Initial Test
    Trying to figure out this plug-in

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$('#map').smoothZoom({
+          width: 100%,
+            height: 100%,
+            pan_BUTTONS_SHOW: "YES",
+            pan_LIMIT_BOUNDARY: "YES",
+            button_SIZE: 24,
+            button_ALIGN: "top right",    
+            zoom_MAX: 200,
+            border_TRANSPARENCY: 20,
+            container: 'zoom_container'
+     })
